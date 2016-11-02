ff906a4faabcbe2e70c0a683c47e90ee93b93167
Nick Schot <nickschot@gmail.com>
2016-11-02 17:42:38 +0100

    Remove unused rollup stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 let pkg = require('./package.json');
@@ -8,7 +7,6 @@ let external = pkg.dependencies ? Object.keys(pkg.dependencies) : [];
 export default {
   entry: 'lib/index.js',
   plugins: [
-    json(),
     nodeResolve({
       module: true,
       jsnext: true,
@@ -21,11 +19,6 @@ export default {
     {
       dest: pkg['main'],
       format: 'cjs',
-      sourceMap: true
-    },
-    {
-      dest: pkg['module'],
-      format: 'es',
       sourceMap: true
     }
   ]
